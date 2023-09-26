449f494ea469ab3ac946c3f22e04c86bad95759f
robot <robot@archies-Mini-2.net>
2023-09-26 22:18:12 +0200

    add functions to analysis pie chart

--- scrape-the-web-python/tasks.py
+++ scrape-the-web-python/tasks.py
@@ -1,11 +1,60 @@
 from robocorp.tasks import task
 from robocorp import browser
 
+from collections import Counter
+from sklearn.cluster import KMeans
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+site_name = "http://www.kermeet.pl"
+output_image = "output/screenshot.png"
+analyzed_image = "output/color_analysis_report.png"
+
 @task
 def open_web_and_screenshot():
     open_the_web()
+    image_analysis()
+    
 
 def open_the_web():
-    browser.goto("http://www.kermeet.pl")
+    browser.goto(site_name)
     page = browser.page()
+    page.screenshot(path=output_image,full_page=True, scale="css")
     
+    
+    #https://towardsdatascience.com/building-an-image-color-analyzer-using-python-12de6b0acf74
+if output_image:
+    image = cv2.imread(output_image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+else:
+    print("no screenshot found")
+    
+def rgb_to_hex(rgb_color):
+    hex_color = "#"
+    for i in rgb_color:
+        i = int(i)
+        hex_color += ("{:02x}".format(i))
+    return hex_color
+
+def prep_image(raw_img):
+    #modified_img = cv2.resize(raw_img, (400, 250), interpolation = cv2.INTER_AREA)
+    modified_img = raw_img.reshape(raw_img.shape[0]*raw_img.shape[1], 3)
+    return modified_img
+
+def color_analysis(img):
+    clf = KMeans(n_clusters = 5)
+    color_labels = clf.fit_predict(img)
+    center_colors = clf.cluster_centers_
+    counts = Counter(color_labels)
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
+    plt.figure(figsize = (12, 8))
+    plt.pie(counts.values(), labels = hex_colors, colors = hex_colors)
+    plt.savefig(analyzed_image)
+    print(hex_colors)
+
+def image_analysis():
+    modified_image = prep_image(image)
+    color_analysis(modified_image)
